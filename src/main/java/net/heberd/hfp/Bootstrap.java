56bac526560f0c2bd057e0c760876805286d40d8
Mohamed Chebbi <mohamed.chebbi@heberd.net>
2026-07-30 08:57:52 +0100

    use Camel Main

--- src/main/java/net/heberd/hfp/Bootstrap.java
+++ src/main/java/net/heberd/hfp/Bootstrap.java
@@ -1,21 +1,18 @@
 package net.heberd.hfp;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.impl.DefaultCamelContext;
-
+import org.apache.camel.main.Main;
 import net.heberd.hfp.adapters.EdifactAdapter;
 
 public class Bootstrap {
 
-	public static void main(String[] args) throws Exception {
-		
-		try (CamelContext ctx = new DefaultCamelContext()) {
-			ctx.addRoutes(new EdifactAdapter());
-			ctx.start();
-			Thread.sleep(10000);
-			ctx.stop();
-		}
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
 
-	}
+        // Add routes directly
+        main.configure().addRoutesBuilder(new EdifactAdapter());
+
+        // Run the Camel application (blocks until shutdown)
+        main.run(args);
+    }
 
 }
